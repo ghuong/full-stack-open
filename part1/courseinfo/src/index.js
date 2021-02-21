51df5b2e0ab01cd96de11e150c6450750daa8576
Gary Huong <garyhuong@gmail.com>
2021-02-20 19:49:32 -0500

    Format src/index.js with Prettier

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+  return <h1>{props.course}</h1>;
+};
 
 const Content = (props) => {
   return (
@@ -14,42 +12,43 @@ const Content = (props) => {
       <Part part={props.parts[1]} />
       <Part part={props.parts[2]} />
     </>
-  )
-}
+  );
+};
 
 const Part = (props) => {
   return (
     <p>
       {props.part.name} {props.part.exercises}
     </p>
-  )
-}
+  );
+};
 
 const Total = (props) => {
-  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
+  const total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <>
@@ -57,7 +56,7 @@ const App = () => {
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
