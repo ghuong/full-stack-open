f4489ce86c98114be082455b015e680b919c6eff
Gary Huong <garyhuong@gmail.com>
2021-02-22 19:21:06 -0500

    Implement Exercise 1.6: unicafe step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Feedback = ({ handleGoodVote, handleNeutralVote, handleBadVote }) => {
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Button handleClick={handleGoodVote} text="good" />
+      <Button handleClick={handleNeutralVote} text="neutral" />
+      <Button handleClick={handleBadVote} text="bad" />
+    </div>
+  );
+};
+
+const Statistics = ({ numGoodVotes, numNeutralVotes, numBadVotes }) => (
+  <div>
+    <h2>Statistics:</h2>
+    <p>good: {numGoodVotes}</p>
+    <p>neutral: {numNeutralVotes}</p>
+    <p>bad: {numBadVotes}</p>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const App = () => {
+  // TODO: save clicks of each button to its own state
+  const [numGoodVotes, setGoodVotes] = useState(0);
+  const [numNeutralVotes, setNeutralVotes] = useState(0);
+  const [numBadVotes, setBadVotes] = useState(0);
+
+  const incrementGoodVotes = () => setGoodVotes(numGoodVotes + 1);
+  const incrementNeutralVotes = () => setNeutralVotes(numNeutralVotes + 1);
+  const incrementBadVotes = () => setBadVotes(numBadVotes + 1);
+
+  return (
+    <>
+      <Feedback
+        handleGoodVote={incrementGoodVotes}
+        handleNeutralVote={incrementNeutralVotes}
+        handleBadVote={incrementBadVotes}
+      />
+      <Statistics
+        numGoodVotes={numGoodVotes}
+        numNeutralVotes={numNeutralVotes}
+        numBadVotes={numBadVotes}
+      />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
