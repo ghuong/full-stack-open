ac2bc5d365daf549cca18f0ca8faa2fe96687175
Gary Huong <garyhuong@gmail.com>
2021-02-26 10:40:16 -0500

    Implement Exercise 2.9*: phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,9 +4,13 @@ import Person from "./components/Person";
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -16,7 +20,7 @@ const App = () => {
       number: newNumber,
     };
 
-    if (persons.find((p) => p.name === newName)) {
+    if (persons.find((p) => p.name.toLowerCase() === newName.toLowerCase())) {
       // person already exists
       alert(`${newName} is already added to the phonebook.`);
     } else {
@@ -28,10 +32,19 @@ const App = () => {
 
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleFilterChange = (event) => setFilter(event.target.value);
+
+  const filteredPersons = filter
+    ? persons.filter((p) => p.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons;
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Filter: <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add a New</h2>
       <form onSubmit={addPerson}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
@@ -45,7 +58,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <Person person={person} key={person.name} />
         ))}
       </ul>
