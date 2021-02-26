1b4b87c7681fb63d74125038c2ceb9163054ef59
Gary Huong <garyhuong@gmail.com>
2021-02-25 19:35:13 -0500

    Implement Exercise 2.1: courseinfo step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import Course from "./components/Course";
 
 const App = () => {
   const course = {
@@ -10,25 +8,22 @@ const App = () => {
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
