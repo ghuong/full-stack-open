7e815bfea6ad9c27c862a1be4719e006f1af6ab4
Gary Huong <garyhuong@gmail.com>
2021-02-27 12:35:39 -0500

    Implement Exercise 2.14*: countries step3

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const CountryInfo = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const WEATHER_API_BASEURL = "http://api.weatherstack.com/";
+    const currentWeatherURL = `${WEATHER_API_BASEURL}current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.capital}`;
+
+    axios.get(currentWeatherURL).then((response) => {
+      setWeather(response.data.current);
+    });
+  }, [country.capital, setWeather]);
+
+  const weatherIcons =
+    weather && weather.weather_icons && weather.weather_icons.length
+      ? weather.weather_icons.map((icon) => (
+          <img src={icon} alt="weather icon" key={icon} />
+        ))
+      : null;
+
+  const weatherInfo = weather ? (
+    <div>
+      <h3>Weather in {country.capital}</h3>
+      <div>
+        <b>Temperature</b>: {weather.temperature} Celsius
+      </div>
+      <div>{weatherIcons}</div>
+      <div>
+        <b>Wind</b>: {weather.wind_speed} mph direction {weather.wind_dir}
+      </div>
+    </div>
+  ) : null;
+
   return (
     <div>
       <h2>{country.name}</h2>
@@ -13,12 +45,9 @@ const CountryInfo = ({ country }) => {
         ))}
       </ul>
       <div>
-        <img 
-          src={country.flag} 
-          alt={`Flag of ${country.name}`} 
-          width="200"
-        />
+        <img src={country.flag} alt={`Flag of ${country.name}`} width="200" />
       </div>
+      {weatherInfo}
     </div>
   );
 };
